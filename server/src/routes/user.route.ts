9666700d004383ac8b892fed50d4c00258060f52
sudarshanbashyal <sudarshanbsyl@gmail.com>
2026-02-27 19:17:11 +0100

    feat:(server) add migration for new models

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -6,6 +6,12 @@ import { getUsers } from "../controllers/user.controller";
 
 const router = Router();
 
-router.get("/users", getUsers);
+router.get("/users", getUsers, (req, res) => {
+  console.log("res: ", res.locals);
+
+  res.json({
+    ok: true,
+  });
+});
 
 export default router;
