ad81e7b82e8aac63c8f2f3e6b2b4161f56d47772
sudarshanbashyal <sudarshanbsyl@gmail.com>
2026-03-03 19:23:42 +0100

    feat: implement password update, auth, profile, and applications endpoints

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,10 +1,14 @@
 // packages
 import { Router } from "express";
 
+// libs
+import { isAuth } from "../lib";
+
 // controllers
 import * as Controller from "../controllers";
 
 export const authRouter = Router();
 
-authRouter.post("/auth/register", Controller.register);
 authRouter.get("/auth/login", Controller.login);
+authRouter.post("/auth/register", Controller.register);
+authRouter.patch("/auth/password", isAuth, Controller.changePassword);
