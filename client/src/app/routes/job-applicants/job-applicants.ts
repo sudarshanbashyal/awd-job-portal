7226d423e0b7875b628d9323c699da5f4eeaf052
sudarshanbashyal <sudarshanbsyl@gmail.com>
2026-03-16 18:10:20 +0100

    feat: add job details for job applicants page

---
+++ client/src/app/routes/job-applicants/job-applicants.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { JobDetails } from '../../components/job-details/job-details';
+
+@Component({
+  selector: 'app-job-applicants',
+  imports: [JobDetails],
+  templateUrl: './job-applicants.html',
+  styleUrl: './job-applicants.scss',
+})
+export class JobApplicants {
+
+}
